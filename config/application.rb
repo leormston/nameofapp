f9313dcb23248d771ca6848aec701fb948661e73
louie <l.e.ormston@gmail.com>
2019-01-02 20:53:15 +0000

    Fixing Compile Error

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Nameofapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
