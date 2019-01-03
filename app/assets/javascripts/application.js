2ab03c674b5e4b2fc9064a44f87f09eb158dc5f0
louie <l.e.ormston@gmail.com>
2019-01-03 23:34:53 +0000

    Visual Update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+
+$("#sidebar_hide_button").on("click", function() {
+  $( "#sidebar" ).css("display", "none");
+  $( "#sidebar_hide_button" ).css("visibility", "hidden");
+  $( "#sidebar_show_button" ).css("visibility", "visible");
+  $( "#pagecontentcustom" ).css("padding-left", "0px");
+})
+
+$("#sidebar_show_button").on("click", function() {
+  $( "#sidebar" ).css("display", "block");
+  $( "#sidebar_hide_button" ).css("visibility", "visible");
+  $( "#sidebar_show_button" ).css("visibility", "hidden");
+  $( "#pagecontentcustom" ).css("padding-left", "253.6px");
+
+
+})
