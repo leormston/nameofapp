8e33beaa3299088c67e135b2445e3af2191f001f
louie <l.e.ormston@gmail.com>
2019-01-15 13:23:51 +0000

    6.5/6.6 Error viewing

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,7 +7,6 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    byebug
     @product = Product.find(params[:product_id])
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
