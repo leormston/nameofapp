8734549ca2bbceaba88cb2d5085b72716dd33a35
louie <l.e.ormston@gmail.com>
2019-01-21 20:22:29 +0000

    6.10 AJAX - Not working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,15 +5,16 @@ class CommentsController < ApplicationController
     @comment.user = current_user
     @comment.save
     redirect_to product_path(@product)
-  end
 
-  respond_to do |format|
-    if @comment.save
-      format.html { redirect_to @product, notice: 'Review was created successfully.'}
-      format.json {render :show, status: :created, location: @product}
-    else
-      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @product, notice: 'Review was created successfully.'}
+        format.json {render :show, status: :created, location: @product}
+        format.js
+      else
+        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
   def destroy
