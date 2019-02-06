4d3a6813e57af96d834d599b296bc981cbefa81a
louie <l.e.ormston@gmail.com>
2019-02-06 19:25:02 +0000

    Javascript tag removed from body

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,21 +17,7 @@
 //= require_tree
 //= require_redis
 
-$("#sidebar_hide_button").on("click", function() {
-  $( "#sidebar" ).css("display", "none");
-  $( "#sidebar_hide_button" ).css("visibility", "hidden");
-  $( "#sidebar_show_button" ).css("visibility", "visible");
-  $( "#pagecontentcustom" ).css("padding-left", "0px");
-})
 
-$("#sidebar_show_button").on("click", function() {
-  $( "#sidebar" ).css("display", "block");
-  $( "#sidebar_hide_button" ).css("visibility", "visible");
-  $( "#sidebar_show_button" ).css("visibility", "hidden");
-  $( "#pagecontentcustom" ).css("padding-left", "253.6px");
-
-
-})
 
 
 $(".alert").delay(1000).fadeOut(1000)
